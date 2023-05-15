114e36ca8200a3c5b12cc4caa27e516d7a51b4f0
analopezsosa <100412590+analopezsosa@users.noreply.github.com>
2023-05-15 18:18:56 +0200

    Primera version boton generar pdf

--- app.py
+++ app.py
@@ -250,6 +250,14 @@ app.layout = html.Div([
                 html.Br(),
                 table_cve
 
+            ]),
+            html.Br(),
+
+            dbc.Row([
+                html.H2("Generar informe"),
+                dbc.Button("Generar Informe", id="boton-generar-informe", color="primary", className="mr-1"),
+                html.Div(id="informe")
+
             ])
 
         ],
@@ -315,6 +323,36 @@ def update_graph(value):
         )
 
 
+@callback(
+	Output('informe', 'children'),
+	Input(component_id='boton-generar-informe', component_property='value')
+)
+def generar_informe():
+    # Crear documento PDF
+    pdf = FPDF()
+    pdf.add_page()
+
+    # Agregar título y subtítulo
+    pdf.set_font("Arial", size=16)
+    pdf.cell(200, 10, txt="Informe de Vulnerabilidades", ln=1, align="C")
+    pdf.set_font("Arial", size=12)
+    pdf.cell(200, 10, txt="Generado automáticamente por Dashboard SI", ln=1, align="C")
+
+    # Agregar gráficos y tablas
+    pdf.cell(200, 10, txt="Top Dispositivos Vulnerables", ln=1)
+    pdf.image("fig_devices.png", x=10, y=40, w=100)
+    pdf.cell(200, 10, txt="Dispositivos más peligrosos", ln=1)
+    pdf.image("fig_most_devices.png", x=10, y=100, w=100)
+    pdf.cell(200, 10, txt="Dispositivos menos peligrosos", ln=1)
+    pdf.image("fig_least_devices.png", x=10, y=160, w=100)
+    pdf.cell(200, 10, txt="Top 10 Direcciones IP Origen", ln=1)
+    pdf.set_font("Arial", size=10)
+    pdf.cell(200, 10, txt=table_ips.to_string(index=False), ln=1)
+    pdf.cell(200, 10, txt="Top 10 CVEs más recientes", ln=1)
+    pdf.cell(200, 10, txt=table_cve.to_string(index=False), ln=1)
+
+    # Guardar PDF
+    pdf.output("informe.pdf")
 
 
 
